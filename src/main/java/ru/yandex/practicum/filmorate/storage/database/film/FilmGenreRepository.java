5b03bd144f5a5652dbefed67a96b16f1085bf8ec
Дмитрий Бражников <brazhnikov.dm@yandex.ru>
2024-09-07 13:14:06 +0400

    Исправления в соответствии с checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/storage/database/film/FilmGenreRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/database/film/FilmGenreRepository.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 @Repository
 public interface FilmGenreRepository extends JpaRepository<FilmGenreDao, Long> {
+
     void deleteAllByFilmId(Long filmId);
     List<FilmGenreDao> findAllByFilmId(Long filmId);
 }
